0be0b4006a4d891513568b88540cafac23dcc9e2
KIS666 <120309902+KIS666@users.noreply.github.com>
2022-12-24 23:43:26 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.IO;
 
-
-
 namespace _1
 {
-   
 
     public  class Order
     {
@@ -37,19 +34,17 @@ namespace _1
     {
         [Newtonsoft.Json.JsonProperty("orders")]
         public static List<Order> Orders { get; set; }
-        
     }
 
     class Program
     {
         static void Main(string[] args)
-        {
-
+        {          
             string path = Path.Combine(Environment.CurrentDirectory, @"input.txt");
             string json = File.ReadAllText(path);
             var rootobject = JsonConvert.DeserializeObject<Root>(value: json);
-            
-            for (int j = 0; j < Root.Orders.Count; j++)
+            int count = Root.Orders.Count;
+            for (int j = 0; j < count; j++)
             {
                 if (String.IsNullOrEmpty(Root.Orders[j].Price))
                 {
@@ -71,22 +66,56 @@ namespace _1
 
                 if (Root.Orders[j].Points.Count > 2)
                 {
-                    List<string> list1 = new List<string>();
-                    for (int i = 0; i < (Root.Orders[j].Points.Count - 1); i++)
-                    {
-                        list1.Add(Root.Orders[j].Points[i]);
-                        list1.Add(Root.Orders[j].Points[i+1]);
-                        list1.Add(Root.Orders[j].Duration);
-                    }
-                    Root.Orders[j].Points = list1;
-                    
-                }
+                    string[] Time = Root.Orders[j].Duration.Split(':');
+                    int sec1 = Convert.ToInt32(Time[0]) * 3600 + Convert.ToInt32(Time[1]) * 60 + Convert.ToInt32(Time[2]);
+                    int sec2 = sec1 / (Root.Orders[j].Points.Count - 1);
+                    int hour = sec2 / 3600;
+                    int min = sec2 % 3600 / 60;
+                    int sec = min / 60;
+                    Root.Orders[j].Duration = $"{hour}:{min}:{sec}";
 
-                //Console.ReadKey();
-                
+                    TimeSpan Time_Razn = new TimeSpan(Convert.ToInt32(hour), Convert.ToInt32(min), Convert.ToInt32(sec));
+                    string[] TimeA = Root.Orders[j].StartTime.Split(' ');
+                    string[] TimeB = Root.Orders[j].FinishTime.Split(' ');
+                    string[] TimeA_0 = TimeA[0].Split('.');
+                    string[] TimeB_0 = TimeB[0].Split('.');
+                    string[] TimeA_1 = TimeA[1].Split(':');
+                    string[] TimeB_1 = TimeB[1].Split(':');
+                    DateTime Time_Start = new DateTime(Convert.ToInt32(TimeA_0[2]), Convert.ToInt32(TimeA_0[1]), Convert.ToInt32(TimeA_0[0]), Convert.ToInt32(TimeA_1[0]), Convert.ToInt32(TimeA_1[1]), Convert.ToInt32(TimeA_1[2]));
+                    DateTime Time_Finish = new DateTime(Convert.ToInt32(TimeB_0[2]), Convert.ToInt32(TimeB_0[1]), Convert.ToInt32(TimeB_0[0]), Convert.ToInt32(TimeB_1[0]), Convert.ToInt32(TimeB_1[1]), Convert.ToInt32(TimeB_1[2]));
+                    var time1 = Convert.ToString(Time_Start.Add(Time_Razn));
+                    var time2 = Convert.ToString(Time_Finish.Subtract(Time_Razn));
+                    
+                    Order newOrd = new Order();
+                    newOrd.Id = Root.Orders[j].Id;
+                    newOrd.Customer = Root.Orders[j].Customer;
+                    newOrd.StartTime = time2;
+                    newOrd.FinishTime = Root.Orders[j].FinishTime;
+                    newOrd.Price = Root.Orders[j].Price;
+                    newOrd.Car = Root.Orders[j].Car;
+                    newOrd.Duration = Root.Orders[j].Duration;
+                    Root.Orders[j].FinishTime = time1;
+
+                    List <List<string>> list1 = new List<List<string>>();
+                    for (int i = 0; i < Root.Orders[j].Points.Count - 1; i++)
+                    {
+                        List<string> list2 = new List<string>();
+                        list2.Add(Root.Orders[j].Points[i]);
+                        list2.Add(Root.Orders[j].Points[i+1]);
+                        list1.Add(list2);
+                    }
+                    
+                    Root.Orders[j].Points = list1[0];
+                    newOrd.Points = list1[1];
+                    Root.Orders.Insert(j + 1, newOrd);
+                    count++;
+                    j++;
+                }
+      
             }
             var json2 = JsonConvert.SerializeObject(Root.Orders, Formatting.Indented);
             File.WriteAllText(@"output.json", json2);
+            //Console.ReadKey();
         }
     }
 }
